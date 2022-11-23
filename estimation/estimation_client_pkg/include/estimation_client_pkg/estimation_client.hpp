170d68b59042f916cb4c02cb70cd9c379f49d7c4
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-11-23 23:16:59 +0900

    add: acc_estimation

--- estimation/estimation_client_pkg/include/estimation_client_pkg/estimation_client.hpp
+++ estimation/estimation_client_pkg/include/estimation_client_pkg/estimation_client.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <common_srvs/SensorService.h>
 #include <common_srvs/VisualizeCloud.h>
+#include <common_srvs/VisualizeImage.h>
 #include <common_srvs/SemanticSegmentationService.h>
 #include <common_srvs/ObjectDetectionService.h>
 #include <common_srvs/IouCalcurateService.h>
@@ -20,6 +21,7 @@ class EstimationClient
 public:
     EstimationClient(ros::NodeHandle &);
     void main();
+    void acc_main(int);
     void set_paramenter();
     XmlRpc::XmlRpcValue param_list;
     int the_number_of_execute_;
@@ -27,9 +29,9 @@ public:
 private:
     ros::NodeHandle nh_, pnh_;
     ros::ServiceClient sensor_client_, object_detect_client_, visualize_client_,
-    cloud_network_client_, accuracy_client_, hdf5_client_;
+    cloud_network_client_, accuracy_client_, hdf5_client_, vis_image_client_;
     std::string sensor_service_name_, object_detect_service_name_, visualize_service_name_,
-    cloud_network_service_name_, accuracy_service_name_, hdf5_service_name_;
+    cloud_network_service_name_, accuracy_service_name_, hdf5_service_name_, vis_image_service_name_;
     std::string world_frame_, sensor_frame_;
     TfBasic tf_basic_;
     UtilMsgData util_msg_data_;
---
+++ estimation/estimation_client_pkg/src/acc_estimation_client.cpp
@@ -0,0 +1,71 @@
+#include <estimation_client_pkg/estimation_client.hpp>
+
+EstimationClient::EstimationClient(ros::NodeHandle &nh) :
+nh_(nh),
+pnh_("~")
+{
+    set_paramenter();
+    object_detect_client_ = nh_.serviceClient<common_srvs::ObjectDetectionService>(object_detect_service_name_);
+    cloud_network_client_ = nh_.serviceClient<common_srvs::SemanticSegmentationService>(cloud_network_service_name_);
+    visualize_client_ = nh_.serviceClient<common_srvs::VisualizeCloud>(visualize_service_name_);
+    vis_image_client_ = nh_.serviceClient<common_srvs::VisualizeImage>(vis_image_service_name_);
+    accuracy_client_ = nh_.serviceClient<common_srvs::IouCalcurateService>(accuracy_service_name_);
+    hdf5_client_ = nh_.serviceClient<common_srvs::Hdf5OpenService>(hdf5_service_name_);
+}
+
+void EstimationClient::set_paramenter()
+{
+    pnh_.getParam("acc_estimation_main", param_list);
+    visualize_service_name_ = static_cast<std::string>(param_list["visualize_service_name"]);
+    vis_image_service_name_ = static_cast<std::string>(param_list["visualize_image_service_name"]);
+    object_detect_service_name_ = static_cast<std::string>(param_list["object_detect_service_name"]);
+    cloud_network_service_name_ = static_cast<std::string>(param_list["cloud_network_service_name"]);
+    accuracy_service_name_ = static_cast<std::string>(param_list["accuracy_service_name"]);
+    hdf5_service_name_ = static_cast<std::string>(param_list["hdf5_service_name"]);
+    the_number_of_execute_ = param_list["the_number_of_execute"];
+}
+
+void EstimationClient::acc_main(int index)
+{
+    common_srvs::Hdf5OpenService hdf5_srv;
+    hdf5_srv.request.index = index;
+    Util::client_request(hdf5_client_, hdf5_srv, hdf5_service_name_);
+    sensor_msgs::Image image = hdf5_srv.response.image;
+    common_msgs::CloudData cloud_data = hdf5_srv.response.cloud_data;
+    common_srvs::ObjectDetectionService ob_detect_2d_srv;
+    ob_detect_2d_srv.request.input_image = hdf5_srv.response.image;
+    Util::client_request(object_detect_client_, ob_detect_2d_srv, object_detect_service_name_);
+    std::vector<common_msgs::BoxPosition> box_pos = ob_detect_2d_srv.response.b_boxs;
+    std::vector<float> cinfo_list = hdf5_srv.response.camera_info;
+    cv::Mat img = UtilMsgData::rosimg_to_cvimg(image, sensor_msgs::image_encodings::BGR8);
+    Get3DBy2D get3d(cinfo_list, Util::get_image_size(img));
+    std::vector<common_msgs::CloudData> cloud_multi = get3d.get_out_data(cloud_data, box_pos);
+    common_srvs::SemanticSegmentationService semantic_srv;
+    semantic_srv.request.input_data_multi = cloud_multi;
+    Util::client_request(cloud_network_client_, semantic_srv, cloud_network_service_name_);
+
+    common_srvs::VisualizeCloud visualize_srv;
+    visualize_srv.request.cloud_data_list.push_back(hdf5_srv.response.cloud_data);
+    visualize_srv.request.topic_name_list.push_back("hdf5_package");
+    Util::client_request(visualize_client_, visualize_srv, visualize_service_name_);
+
+    common_srvs::VisualizeImage vis_img_srv;
+    vis_img_srv.request.image_list.push_back(hdf5_srv.response.image);
+    vis_img_srv.request.topic_name_list.push_back("hdf5_image");
+    Util::client_request(vis_image_client_, vis_img_srv, vis_image_service_name_);
+    // for (int i = 0; i < semantic_srv.response.output_data_multi.size(); i++) {
+    //     visualize_srv.request.topic_name_list.push_back("cloud_multi_" + std::to_string(i));
+    // }
+    
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "estimation_client");
+    ros::NodeHandle nh;
+    EstimationClient estimation_client(nh);
+    for (int i = 0; i < estimation_client.the_number_of_execute_; i++) {
+        estimation_client.acc_main(i+1);
+        ros::Duration(6).sleep();
+    }
+}
