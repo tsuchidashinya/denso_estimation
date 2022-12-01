ce410966703706e71506e34df18a81b56c0c63be
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-12-01 19:14:13 +0000

    fix: demo.sh

--- network/src/network/object_detection/ssd/demo.sh
+++ network/src/network/object_detection/ssd/demo.sh
@@ -4,6 +4,6 @@ python3 /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network
 --config-file /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network/src/network/object_detection/ssd/config/yaml/vgg_ssd512_voc0712.yaml \
 --checkpoints_dir /home/ericlab/tsuchida/2022_11/checkpoints/SSD \
 --output_dir /home/ericlab/tsuchida/2022_11/annotation/YOLO/retry_1000/output \
---score_threshold 0.4
+--score_threshold 0.9
 # --num_epoch 100 \
 # --save_step 20 
