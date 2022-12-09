d4419d4f1a0476e6e8f4f076e3fd21fe44a39d01
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-12-09 21:53:09 +0000

    fix: default

--- network/src/network/object_detection/ssd/train.sh
+++ network/src/network/object_detection/ssd/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python3 /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network/src/network/object_detection/ssd/train.py \
---dataset_dir /home/ericlab/tsuchida/2022_12/annotation/ObjectDetection/SSD/multi_object/object_bbox \
+--dataset_dir /home/ericlab/tsuchida/2022_12/annotation/ObjectDetection/SSD/multi_object/object_bbox_1 \
 --config-file /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network/src/network/object_detection/ssd/config/yaml/vgg_ssd512_voc0712.yaml \
 --checkpoints /home/ericlab/tsuchida/2022_12/checkpoints/SSD/latest_method \
 # --num_epoch 100 \
