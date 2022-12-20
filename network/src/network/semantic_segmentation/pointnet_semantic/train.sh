0f2215db16f39d81a1e82fe89e411e0b3064390d
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-12-20 15:41:44 +0000

    fix: train.sh

--- network/src/network/semantic_segmentation/pointnet_semantic/train.sh
+++ network/src/network/semantic_segmentation/pointnet_semantic/train.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 python3 /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network/src/network/semantic_segmentation/pointnet_semantic/train.py \
---dataset_path /home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object_kai/kai3228/kai_1.hdf5 \
---checkpoints /home/ericlab/tsuchida/2022_12/checkpoints/Semseg/multi_object_kai_2000 \
+--dataset_path /home/ericlab/tsuchida/2022_12/annotation/Semseg/data_num_experiment/data_4000/sensor_b_box_4000.hdf5 \
+--checkpoints /home/ericlab/tsuchida/2022_12/checkpoints/Semseg/実験1/data_4000 \
 --batch_size 1 \
---num_epoch 200 \
+--num_epoch 120 \
 --save_epoch_freq 20 \
 --lr 0.0001 \
 --num_instance_classes 2 \
---start_index 1
+--start_index 1 \
+--weights /home/ericlab/Downloads/data_4000/latest.pth
