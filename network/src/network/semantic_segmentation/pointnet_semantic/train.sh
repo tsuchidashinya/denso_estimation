45a87c7e993f698a59737cdc3d866c66a0d03bfc
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2023-01-08 15:47:24 +0000

    fix: acc_estimation_client_real

--- network/src/network/semantic_segmentation/pointnet_semantic/train.sh
+++ network/src/network/semantic_segmentation/pointnet_semantic/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python3 /home/ericlab/tsuchida/ros_package/study_ws/src/denso_estimation/network/src/network/semantic_segmentation/pointnet_semantic/train.py \
---dataset_path /home/ericlab/Downloads/sensor_b_box_2000_noize_max_improve.hdf5 \
---checkpoints /home/ericlab/tsuchida/2023_01/checkpoints/Semseg/HV8/sensor_b_box_2000_noize_max_improve \
+--dataset_path /home/ericlab/tsuchida/2023_01/annotation/Semseg/HV8/sensor_b_box_2000_improve.hdf5 \
+--checkpoints /home/ericlab/tsuchida/2023_01/checkpoints/Semseg/HV8/sensor_b_box_2000_improve \
 --batch_size 1 \
 --num_epoch 200 \
 --save_epoch_freq 20 \
